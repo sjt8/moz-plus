47a0bf592695469766ed049d4663c7518ed89112
zie2203 <zeba.sudheer@gmail.com>
2023-10-10 17:47:40 +0530

    movie-details

--- moz_plus_project/subscriber/urls.py
+++ moz_plus_project/subscriber/urls.py
@@ -7,5 +7,8 @@ app_name = 'subscriber'
 urlpatterns = [
     path('subscriber/', views.subscriber, name='subscriber'),
     path('subscriber/series/', views.series, name='series'),
+    path('subscriber/movies/', views.movies, name='movies'),
+    path('subscriber/mylist/', views.watchlist, name='mylist'),
+    path('subscriber/movie_info/<int:passed_id>/', views.movie_info, name='movie_info'),
     path('subscriber/series/<int:series_id>/', views.series_details, name='series_details'),
 ]
--- moz_plus_project/subscriber/views.py
+++ moz_plus_project/subscriber/views.py
@@ -5,9 +5,52 @@ from . import models
 
 
 # Create your views here.
+def homepage(request):
+    movies = apps.get_model('content_creator', 'Movie').objects.all()
+    series = apps.get_model('content_creator', 'Series').objects.all()
+
+    context = {
+        'movies': movies,
+        'series': series,
+    }
+
+    return render(request, 'homepage.html', context)
+
+
+def movies(request):
+    content_movies = apps.get_model(app_label='content_creator', model_name='Movie').objects.all()
+    continue_movies = apps.get_model(app_label='content_management', model_name='ContinueWatchingMoviePart').objects.all()
+    recommended_movies = apps.get_model(app_label='content_management', model_name='FavouriteMovie').objects.all()
+    trending_movies = apps.get_model(app_label='content_management', model_name='ShowCollectionMovie').objects.all()
+
+    context = {
+        'content_movies': content_movies,
+        'continue_movies':continue_movies,
+        'recommended_movies':recommended_movies,
+        'trending_movies':trending_movies,
+    }
+    return render(request, 'subscriber/movies.html', context)
+
+
+def movie_info(request,passed_id):
+    content_movies = apps.get_model(app_label='content_creator', model_name='Movie').objects.get(id=passed_id)
+    content_movies_part = apps.get_model(app_label='content_creator', model_name='MoviePart').objects.filter(movie_id=passed_id)
+    movie_part_id = request.GET.get('movie-part') or content_movies_part.first().id
+    content_movie_trailer = apps.get_model(app_label='content_creator', model_name='MovieTrailer').objects.filter(movie_part_id=movie_part_id)
+
+    context = {
+        'content_movies': content_movies,
+        'content_movies_part': content_movies_part,
+        'content_movie_trailer': content_movie_trailer,
+    }
+    return render(request, 'subscriber/movie_info.html', context)
+
 def subscriber(request):
     return render(request, 'subscriber/subscriber.html')
+def watchlist(request):
+    mylist = apps.get_model('content_management', 'Watchlist').objects.all()
 
+    return render(request, "mylist.html", {"mylist": mylist, })
 
 def series(request):
 
