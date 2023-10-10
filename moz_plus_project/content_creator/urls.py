ac57b52be06c296dd658e533c24c92c0770e0b56
unknown <vishnupriyaha2016@gmail.com>
2023-10-10 15:36:53 +0530

    add,edit,delete of series

--- moz_plus_project/content_creator/urls.py
+++ moz_plus_project/content_creator/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-
+from .views import add_season,add_episode,edit_season,delete_Season,delete_episode,add_series,edit_episode
 from . import views
 
 app_name = 'content_creator'
@@ -8,4 +8,12 @@ urlpatterns = [
     path('content-creator/', views.content_creator, name='content_creator'),
     path('content-creator/series/', views.series, name='series'),
     path('content-creator/series/<int:series_id>/', views.series_details, name='series_details'),
+    path('add-season/',add_season, name='add_season'),
+    path('add-episode/',add_episode, name='add_episode'),
+    path("edit-season/<int:passed_id>/",edit_season, name='edit_season'),
+    path("delete_season/<int:season_id>/", delete_Season, name='delete_season'),
+    path("delete_episode/<int:episode_id>/", delete_episode, name='delete_episode'),
+    path('add-series/',add_series, name='add_series'),
+    path("edit-episode/<int:passed_id>/", edit_episode, name='edit_episode'),
+
 ]
--- moz_plus_project/content_creator/views.py
+++ moz_plus_project/content_creator/views.py
@@ -26,12 +26,109 @@ def series(request):
 def series_details(request, series_id):
     content_series = models.Series.objects.get(id=series_id)
     content_season = models.Season.objects.filter(series_id=series_id)
-    season_id = request.GET.get('season') or content_season.first().id
+
+    if request.GET.get('season'):
+        season_id = request.GET.get('season')
+    elif content_season.first():
+        season_id = content_season.first().id
+    else:
+        season_id = 0
+
     content_episodes = models.Episodes.objects.filter(season_id=season_id)
 
     context = {
         'content_series': content_series,
         'content_season': content_season,
         'content_episodes': content_episodes,
+        'season_id': season_id,
     }
     return render(request, 'content_creator/series_details.html', context)
+
+
+
+
+def add_season(request):
+    if request.method == 'POST':
+        add_season_form = SeasonForm(request.POST, request.FILES)
+        if add_season_form.is_valid():
+            new_season = add_season_form.save(commit=False)
+            new_season.post_author = request.user
+            new_season.save()
+            return redirect('content_creator:series_details',new_season.series.id)
+
+    else:
+        add_season_form = SeasonForm()
+
+    return render(request, 'content_creator/add_season.html', context={'add_season_form': add_season_form})
+
+def add_episode(request):
+    if request.method == 'POST':
+        add_episode_form = EpisodesForm(request.POST, request.FILES)
+        if add_episode_form.is_valid():
+            new_episode = add_episode_form.save(commit=False)
+            new_episode.post_author = request.user
+            new_episode.save()
+            return redirect('content_creator:series_details',new_episode.season.series.id)
+
+    else:
+        add_episode_form = EpisodesForm()
+
+    return render(request, 'content_creator/add_episode.html', context={'add_episode_form': add_episode_form})
+
+
+def edit_season(request, passed_id):
+    # get the get method var and passing  that along with the model
+    season_details = get_object_or_404(Season,id=passed_id)
+    edit_season_form =EditSeasonForm (request.POST or None, request.FILES or None,instance=season_details)
+    if edit_season_form.is_valid():
+        new_season = edit_season_form.save(commit=False)
+        new_season.save()
+        return redirect('content_creator:series_details', new_season.series.id)
+
+    return render(request, 'content_creator/edit_season.html', context={'edit_season_form': edit_season_form})
+
+def edit_episode(request, passed_id):
+    # get the get method var and passing  that along with the model
+    episode_details = Episodes.objects.get(id=passed_id)
+    edit_episode_form =EditEpisodesForm (request.POST or None, request.FILES or None,instance=episode_details)
+    if edit_episode_form.is_valid():
+        new_episode=edit_episode_form.save(commit=False)
+        new_episode.save()
+        return redirect('content_creator:series_details', new_episode.season.series.id)
+
+    return render(request, 'content_creator/edit_episode.html', context={'edit_episode_form': edit_episode_form})
+
+
+def delete_Season(request, season_id):
+    content_season = models.Season.objects.get(id=season_id)
+    series_id = content_season.series.id
+    content_season.delete()
+    return redirect('content_creator:series_details', series_id)
+
+
+# deleting the episode
+
+def delete_episode(request, episode_id):
+    # get the get method var and passing  that along with the model
+    content_episode = models.Episodes.objects.get(id=episode_id)
+    series_id = content_episode.season.series.id
+    content_episode.delete()
+    return redirect('content_creator:series_details',series_id)
+
+
+
+
+
+
+def add_series(request):
+    if request.method == 'POST':
+        add_series_form =SeriesForm(request.POST, request.FILES)
+        if add_series_form.is_valid():
+            new_series = add_series_form.save(commit=False)
+            new_series.post_author = request.user
+            new_series.save()
+            return redirect('content_creator:series')
+    else:
+        add_series_form = SeriesForm()
+
+    return render(request, 'content_creator/add_series.html', {'add_series_form': add_series_form })
