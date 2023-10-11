78754a03a372258ed76ebcd30590721a3c830054
sj <135508922+sjt8@users.noreply.github.com>
2023-10-11 16:07:02 +0530

    added super admin

--- moz_plus_project/super_admin/views.py
+++ moz_plus_project/super_admin/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.apps import apps
+
 
 # Create your views here.
+def admin_super(request):
+    menus = ['Country', 'Language']
+    active = request.GET.get('active') or menus[0]
+    values = apps.get_model('super_admin', active).objects.all()
+
+    context = {
+        'menus': menus,
+        'active': active,
+        'values': values
+    }
+    return render(request, 'super_admin/super_admin.html', context)
