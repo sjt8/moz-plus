729c8c63465dbd398751daa7056409413f42c4f3
sj <135508922+sjt8@users.noreply.github.com>
2023-12-10 17:39:58 +0530

    remove debug property value

--- moz_plus_project/moz_plus_project/settings.py
+++ moz_plus_project/moz_plus_project/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gt+-n*62eb1+@!493+i5=@x5*_@8x92ozzi%s!lm46#s!elsik'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG")
 
 ALLOWED_HOSTS = []
 
