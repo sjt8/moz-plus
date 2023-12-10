515883167352840bb9a8de1e1ed20e5441b967e1
sj <135508922+sjt8@users.noreply.github.com>
2023-12-10 22:58:54 +0530

    fixed debug property

--- moz_plus_project/moz_plus_project/settings.py
+++ moz_plus_project/moz_plus_project/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DEBUG")
+DEBUG = True if config("DEBUG") == "True" else False
 
 ALLOWED_HOSTS = []
 
